163caeab16940e34e20ec1e3ba7218c6665ee23d
Jimmbo19 <jega190112@gmail.com>
2024-10-12 20:48:46 +0000

    login y signup

--- src/api/routes.py
+++ src/api/routes.py
@@ -6,6 +6,11 @@ from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
@@ -20,3 +25,37 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/login', methods=['POST'])
+def login():
+
+    email=request.json.get("email",None)
+    password=request.json.get("password",None)
+
+    user=User.query.filter_by(email=email).first()
+
+    if user== None:
+        return jsonify({"msg": "could not find the email"}), 401
+    if email!= user.email or password != user.password:
+        return jsonify({"msg": "bad email or password"}), 401
+    
+    access_token =create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+@api.route('/signup', methods=['POST'])
+def signup():
+    body = request.get_json()
+    
+    user = User.query.filter_by(email=body["email"]).first()
+   
+    if user is None:
+        user = User(email=body["email"], password=body["password"], is_active=True)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"msg": "Usuario creado exitosamente"}), 200
+    else:
+        return jsonify({"msg": "Ya existe ese usuario"}), 401
+
+    
+
+
